682c5262b5b55baeb36231dfa16f78cd05dc459d
SynnK <synnkfps@gmail.com>
2023-08-02 16:38:25 -0300

    better keybind handling

--- src/main/java/me/synnk/MiningWare.java
+++ src/main/java/me/synnk/MiningWare.java
@@ -1,8 +1,11 @@
 package me.synnk;
 
+import me.synnk.config.Config;
 import me.synnk.features.*;
 import me.synnk.handlers.ChatEventHandler;
 import me.synnk.managers.FeatureManager;
+import me.synnk.managers.KeyBindingManager;
+import me.synnk.utils.Log;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.common.MinecraftForge;
@@ -18,15 +21,15 @@ import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixins;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
 @Mod(modid = "miningware", useMetadata = true, clientSideOnly = true, name = "MiningWare", acceptedMinecraftVersions = "1.8.9", version = "1.0.0")
 public class MiningWare {
 
-    public static final String MODNAME = "@NAME@";
-    public static final String VERSION = "@VERSION@";
-    public static final String MODID = "@MODID";
-
-    public static KeyBinding[] keyBinds = new KeyBinding[2];
+    public static final String MODNAME = "MiningWare";
+    public static final String VERSION = "1.0.0";
+    public static final String MODID = "miningware";
 
 
     @Mod.Instance(MODID)
@@ -52,15 +55,16 @@ public class MiningWare {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new ChatEventHandler());
 
-
-        keyBinds[0] = new KeyBinding("Open GUI", Keyboard.KEY_NONE, "MiningWare");
-        keyBinds[1] = new KeyBinding("Lock Yaw", Keyboard.KEY_NONE, "MiningWare");
-
-        for (KeyBinding keyBind : keyBinds) {
-            ClientRegistry.registerKeyBinding(keyBind);
-        }
-
-
+        Config.settingNames.forEach(name -> {
+            KeyBindingManager.addKeybind(name, Keyboard.KEY_NONE, MiningWare.MODNAME);
+            new Log("Adding keybind: " + name);
+        });
+//
+//        // Retrieve the key bindings from KeyBindingManager and register them
+//        List<KeyBinding> keyBindings = KeyBindingManager.getKeyBindings();
+//        for (KeyBinding keyBind : keyBindings) {
+//            ClientRegistry.registerKeyBinding(keyBind);
+//        }
     }
 
     @Mod.EventHandler
@@ -70,13 +74,7 @@ public class MiningWare {
 
     @SubscribeEvent
     public void onKey(InputEvent.KeyInputEvent event) {
-        if (keyBinds[0].isPressed()) {
-            // todo
-        }
-
-        if (keyBinds[1].isPressed()) {
-            new LockYaw().onTrigger();
-        }
+        // todo so we use it on KeyBindingManager instead
     }
 
     @SubscribeEvent
